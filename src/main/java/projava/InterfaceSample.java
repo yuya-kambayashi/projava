942794ab1aaa316219e5d2ff390e6bd9c1bf65ed
yuya-kambayashi <kambayashi73@gmail.com>
2023-09-11 15:15:34 +0900

    use Interface

--- src/main/java/projava/InterfaceSample.java
+++ src/main/java/projava/InterfaceSample.java
@@ -3,16 +3,24 @@ package projava;
 import java.util.List;
 
 public class InterfaceSample {
-    record Student(String name, int score){}
-    record Teacher(String name, String subject){}
+    record Student(String name, int score) implements Named{}
+    record Teacher(String name, String subject) implements Named{}
+
+
+    interface Named {
+        String name();
+    }
 
     public static void main(String args[]){
         var people = List.of(new Student("kis", 80), new Teacher("kam", "Math"));
 
         for(var p : people){
-            var n = p instanceof Student s ? s.name() :
-                    p instanceof Teacher t ? t.name() :
-                            "---";
+//            var n = p instanceof Student s ? s.name() :
+//                    p instanceof Teacher t ? t.name() :
+//                            "---";
+
+            var n = p.name();
+
             System.out.println("Hello Mr.%s ".formatted(n));
         }
     }
